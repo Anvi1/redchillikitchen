674dc200fa8f3803786fa6c26177500b431cf9e8
Anvi1 <anvitag17@gmail.com>
2018-05-16 23:57:08 +0530

    User Details navigation err-Anvita

--- redchillyapp/src/app/app.routes.ts
+++ redchillyapp/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { ChillyHomeComponent } from "../pages/chilly-home/chilly-home";
+import { UserDetailsPage } from "../pages/user-details/user-details";
 
 export const appRoutes = [
   { path: 'chillyhome', component: ChillyHomeComponent },
+  { path: 'userdetails', component: UserDetailsPage },
 ]
--- redchillyapp/src/pages/components.module.ts
+++ redchillyapp/src/pages/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ChillyHomeComponent } from './chilly-home/chilly-home';
+import { UserDetailsPage } from "../pages/user-details/user-details";
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 @NgModule({
-	declarations: [ChillyHomeComponent],
-	imports: [],
-	exports: [ChillyHomeComponent]
+	declarations: [ChillyHomeComponent,UserDetailsPage],
+	imports: [IonicModule],
+	exports: [ChillyHomeComponent,UserDetailsPage]
 })
 export class PagesModule {}
