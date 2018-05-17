5aa0331a945b6bbea88128c6b472b97f37d18555
AG <anvitag17@gmail.com>
2018-05-18 02:17:47 +0530

    U002 - changes in menuitem model

--- redchillyserver/models/menuitem.js
+++ redchillyserver/models/menuitem.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MenuItem = new Schema({
-    id: { type: String, default: '' },
+const menuItem = new Schema({
+    _id: mongoose.Types.ObjectId,
     name: { type: String, default: '' },
     price: { type: Number },
     numbersAddedToCart: { type: Number, default: 0 },
     description: { type: String, default: '' },
+    category: { type: String, default: '' }
 })
+
+const model = mongoose.model('MenuItem', menuItem);
+
+module.exports = model;
