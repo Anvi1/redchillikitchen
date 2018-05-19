099cc3ae57d4e53e753f15e4e6813bc8298f6f9a
AG <anvitag17@gmail.com>
2018-05-19 11:22:23 +0530

    U004 - Source formatting

--- redchillyapp/src/pages/user-details/user-details.ts
+++ redchillyapp/src/pages/user-details/user-details.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the UserDetailsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage()
 @Component({
   selector: 'page-user-details',
   templateUrl: 'user-details.html',
 })
 export class UserDetailsPage {
-  private userDetails : FormGroup;
-  constructor(public navCtrl: NavController, public navParams: NavParams,  private formBuilder: FormBuilder) {
+  private userDetails: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
     this.userDetails = this.formBuilder.group({
       fullName: ['', Validators.required],
       fullAdress: ['', Validators.required],
@@ -24,12 +21,7 @@ export class UserDetailsPage {
     });
   }
 
-  logForm(){
+  logForm() {
     console.log(this.userDetails.value)
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserDetailsPage');
-  }
-
 }
