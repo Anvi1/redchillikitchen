0fd65ea7d41aff14cfd740ccbe3209cf4bfab443
Anvi1 <anvitag17@gmail.com>
2018-05-19 15:42:02 +0530

    Validations

--- redchillyapp/src/pages/user-details/user-details.ts
+++ redchillyapp/src/pages/user-details/user-details.ts
@@ -17,17 +17,19 @@ export class UserDetailsPage {
     this.userDetails = new FormGroup({
       AdressLine1: new FormControl('AdressLine1'),
       AdressLin21: new FormControl('AdressLine2'),
-      AdressLine3: new FormControl('AdressLine3'), 
-     
-  });
+      AdressLine3: new FormControl('AdressLine3'),
+
+    });
 
 
     this.userDetails = this.formBuilder.group({
       fullName: ['', Validators.required],
-      AdressLine1:['',Validators.required],
-      AdressLine2:['',Validators.required],
-      AdressLine3:['',Validators.required],
-      contactNum: ['', Validators.required],
+      AdressLine1: ['', Validators.required],
+      AdressLine2: ['', Validators.required],
+      AdressLine3: ['', Validators.required],
+      contactNum: new FormControl('', Validators.compose([
+        Validators.maxLength(10), Validators.minLength(10), Validators.required
+      ])),
       emailId: new FormControl('', Validators.compose([
         Validators.required,
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
