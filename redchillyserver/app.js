fb3816782f5437d9f9fe812339c58776cd164b96
prataphaldar <prataphaldar1987@gmail.com>
2018-05-31 22:57:03 +0530

    variable route

--- redchillyserver/app.js
+++ redchillyserver/app.js
@@ -9,7 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const menuRoutes = require('./routes/menu');
 const orderRoutes = require('./routes/order');
-
+const variableRoutes = require('./routes/variable');
 const app = express();
 const env = `development`;
 
@@ -34,5 +34,6 @@ app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/menu', menuRoutes);   
 app.use('/api/order', orderRoutes);
+app.use('/api/projectvariable', variableRoutes);
 
 module.exports = app;
